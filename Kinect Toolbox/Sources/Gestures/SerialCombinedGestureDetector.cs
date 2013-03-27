ff8a83df0f974cf6179ef466a660c038c0baeecf
Scott C. <stinkydragonxiii@gmail.com>
2013-03-27 16:21:51 -0400

    Kinect toolbox

---
+++ Kinect Toolbox/Sources/Gestures/SerialCombinedGestureDetector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kinect.Toolbox
+{
+    public class SerialCombinedGestureDetector : CombinedGestureDetector
+    {
+        DateTime? previousGestureTime;
+        List<string> detectedGesturesName = new List<string>();
+
+        public SerialCombinedGestureDetector(double epsilon = 1000)
+            : base(epsilon)
+        {
+        }
+
+        protected override void CheckGestures(string gesture)
+        {
+            if (!previousGestureTime.HasValue)
+            {
+                previousGestureTime = DateTime.Now;
+                detectedGesturesName.Clear();
+            }
+
+            if (!detectedGesturesName.Contains(gesture))
+            {
+                var currentTime = DateTime.Now;
+
+                if (currentTime.Subtract(previousGestureTime.Value).TotalMilliseconds > Epsilon)
+                {
+                    previousGestureTime = null;
+                    return;
+                }
+
+                previousGestureTime = currentTime;
+
+                detectedGesturesName.Add(gesture);
+            }
+            else
+            {
+                previousGestureTime = null;
+                return;
+            }
+
+            if (detectedGesturesName.Count == GestureDetectorsCount)
+            {
+                RaiseGestureDetected(string.Join(">", detectedGesturesName));
+                previousGestureTime = null;
+            }
+        }
+    }
+}
