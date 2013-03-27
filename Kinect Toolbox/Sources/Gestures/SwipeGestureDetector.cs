21b395febb25f378be40bffa82331b70d5d6c122
Jithu Maliakal <jithu.maliakal@gmail.com>
2013-03-27 17:17:15 -0400

    Adding Window Snap test code

--- Kinect Toolbox/Sources/Gestures/SwipeGestureDetector.cs
+++ Kinect Toolbox/Sources/Gestures/SwipeGestureDetector.cs
@@ -1,6 +1,24 @@
 ﻿using System;
 using Microsoft.Kinect;
 
+// Does this work??!
+using Winuser;
+
+/// <summary>
+/// The MoveWindow function changes the position and dimensions of the specified window. For a top-level window, the position and dimensions are relative to the upper-left corner of the screen. For a child window, they are relative to the upper-left corner of the parent window's client area.
+/// </summary>
+/// <param name="hWnd">Handle to the window.</param>
+/// <param name="X">Specifies the new position of the left side of the window.</param>
+/// <param name="Y">Specifies the new position of the top of the window.</param>
+/// <param name="nWidth">Specifies the new width of the window.</param>
+/// <param name="nHeight">Specifies the new height of the window.</param>
+/// <param name="bRepaint">Specifies whether the window is to be repainted. If this parameter is TRUE, the window receives a message. If the parameter is FALSE, no repainting of any kind occurs. This applies to the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent window uncovered as a result of moving a child window.</param>
+/// <returns>If the function succeeds, the return value is nonzero.
+/// <para>If the function fails, the return value is zero. To get extended error information, call GetLastError.</para></returns>
+[DllImport("user32.dll", SetLastError = true)]
+internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+
 namespace Kinect.Toolbox
 {
     public class SwipeGestureDetector : GestureDetector
@@ -46,6 +64,10 @@ namespace Kinect.Toolbox
 
         protected override void LookForGesture()
         {
+
+            HWND WINAPI currentWindow = GetActiveWindow();
+
+
             // Swipe to right
             if (ScanPositions((p1, p2) => Math.Abs(p2.Y - p1.Y) < SwipeMaximalHeight, // Height
                 (p1, p2) => p2.X - p1.X > -0.01f, // Progression to right
@@ -53,7 +75,10 @@ namespace Kinect.Toolbox
                 SwipeMininalDuration, SwipeMaximalDuration)) // Duration
             {
                 RaiseGestureDetected("SwipeToRight");
+                MoveWindow(currentWindow, 720, 450, 640, 480, TRUE);
+
                 return;
+
             }
 
             // Swipe to left
@@ -63,6 +88,8 @@ namespace Kinect.Toolbox
                 SwipeMininalDuration, SwipeMaximalDuration))// Duration
             {
                 RaiseGestureDetected("SwipeToLeft");
+                MoveWindow(currentWindow, 306, 225, 640, 480, TRUE);
+
                 return;
             }
         }
